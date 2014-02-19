8ed907c7d15c7f389bbccd41d03bd7760b45291b
bmontes <bmontes@users.noreply.github.com>
2014-02-19 16:03:51 +0800

    Update drbd script with wait-connect

--- t3/kickstarter/5-2node-drbd.sh
+++ t3/kickstarter/5-2node-drbd.sh
@@ -9,15 +9,17 @@ if [ $? -ne 0 ]; then
 fi
 
 $DRBDADM create-md r{0..1}
+modproeb drbd
+$DRBDADM up r{0..1}
+echo "Waiting for resources to connect..."
+$DRBDADM wait-connect r{0..1}
+if [ $? -ne 0 ]; then
+	echo "Timed out waiting for resources to connect!!"
+	exit 1;
+fi
 
 # Force node sa to be primary
 MYNAME="`uname -n`"
 if [ "$(echo $MYNAME | gawk -F'.' '{print $1}')" = "sa" ]; then
-	modprobe drbd
-	$DRBDADM attach r{0..1}
 	$DRBDADM -- --clear-bitmap new-current-uuid r{0..1}
-	$DRBDADM new-current-uuid r{0..1}
-else
-	service drbd start #If node b, simply start drbd and connect to sa
-	$DRBDADM up r{0..1}
 fi
