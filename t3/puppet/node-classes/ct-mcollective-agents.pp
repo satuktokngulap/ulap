4ea793e98542f167f2619cdcb782ea31f175a3ff
Marc Angco <marc.angco@gmail.com>
2014-03-01 22:02:11 +0800

    Lint Puppet manifests for node classes

--- t3/puppet/node-classes/ct-mcollective-agents.pp
+++ t3/puppet/node-classes/ct-mcollective-agents.pp
@@ -1,11 +1,11 @@
 class ct-mcollective-agents {
     file {'/root/mcollective/':
-        ensure => directory,
-        mode => 0750,
-        owner => root,
-        group => root,
+        ensure  => directory,
+        mode    => '0750',
+        owner   => root,
+        group   => root,
     }
-    
+
     # Remove old packages
 #    exec {'remove-old-agents':
 #        command => 'yum remove -y mcollective-backup-agent mcollective-backup-common mcollective-backup-app',
@@ -14,63 +14,68 @@ class ct-mcollective-agents {
 #        }
 
     file {'/root/mcollective/mcollective-backup-agent-1.2-1.noarch.rpm':
+        ensure  => file,
+        mode    => '0750',
+        owner   => root,
+        group   => root,
+        source  => 'puppet:///modules/ct-files/mcollective/mcollective-backup-agent-1.2-1.noarch.rpm',
         require => File['/root/mcollective/'],
-        ensure => file,
-        mode => 0750,
-        owner => root,
-        group => root,
-        source => "puppet:///modules/ct-files/mcollective/mcollective-backup-agent-1.2-1.noarch.rpm",
     }
 
     file {'/root/mcollective/mcollective-backup-common-1.2-1.noarch.rpm':
-        ensure => file,
-        mode => 0750,
-        owner => root,
-        group => root,
-        source => "puppet:///modules/ct-files/mcollective/mcollective-backup-common-1.2-1.noarch.rpm",
+        ensure  => file,
+        mode    => '0750',
+        owner   => root,
+        group   => root,
+        source  => 'puppet:///modules/ct-files/mcollective/mcollective-backup-common-1.2-1.noarch.rpm',
     }
+
     file {'/root/mcollective/mcollective-backup-app-1.2-1.noarch.rpm':
-        ensure => file,
-        mode => 0750,
-        owner => root,
-        group => root,
-        source => "puppet:///modules/ct-files/mcollective/mcollective-backup-app-1.2-1.noarch.rpm",
+        ensure  => file,
+        mode    => '0750',
+        owner   => root,
+        group   => root,
+        source  => 'puppet:///modules/ct-files/mcollective/mcollective-backup-app-1.2-1.noarch.rpm',
     }
- file {'/root/mcollective/mcollective-backup-package-1.0-1.noarch.rpm':
-        ensure => file,
-        mode => 0750,
-        owner => root,
-        group => root,
-        source => "puppet:///modules/ct-files/mcollective/mcollective-backup-package-1.0-1.noarch.rpm",
+
+    file {'/root/mcollective/mcollective-backup-package-1.0-1.noarch.rpm':
+        ensure  => file,
+        mode    => '0750',
+        owner   => root,
+        group   => root,
+        source  => 'puppet:///modules/ct-files/mcollective/mcollective-backup-package-1.0-1.noarch.rpm',
     }
 
     package {'mcollective-backup-agent':
-        require => File['/root/mcollective/mcollective-backup-agent-1.2-1.noarch.rpm'],
-        ensure => absent,
-        source => "/root/mcollective/mcollective-backup-agent-1.2-1.noarch.rpm",
-        provider => rpm,
-        notify => Service['mcollective'],    
+        ensure      => absent,
+        source      => '/root/mcollective/mcollective-backup-agent-1.2-1.noarch.rpm',
+        provider    => rpm,
+        notify      => Service['mcollective'],
+        require     => File['/root/mcollective/mcollective-backup-agent-1.2-1.noarch.rpm'],
     }
+
     package {'mcollective-backup-common':
-        require => File['/root/mcollective/mcollective-backup-common-1.2-1.noarch.rpm'],
-        ensure => absent,
-        source => "/root/mcollective/mcollective-backup-common-1.2-1.noarch.rpm",
-        provider => rpm,
-        notify => Service['mcollective'],
+        ensure      => absent,
+        source      => '/root/mcollective/mcollective-backup-common-1.2-1.noarch.rpm',
+        provider    => rpm,
+        notify      => Service['mcollective'],
+        require     => File['/root/mcollective/mcollective-backup-common-1.2-1.noarch.rpm'],
     }
+
     package {'mcollective-backup-app':
-        require => [File['/root/mcollective/mcollective-backup-app-1.2-1.noarch.rpm'],],
-        ensure => absent,
-        source => "/root/mcollective/mcollective-backup-app-1.2-1.noarch.rpm",
-        provider => rpm,
-        notify => Service['mcollective'],
+        ensure      => absent,
+        source      => '/root/mcollective/mcollective-backup-app-1.2-1.noarch.rpm',
+        provider    => rpm,
+        notify      => Service['mcollective'],
+        require     => [File['/root/mcollective/mcollective-backup-app-1.2-1.noarch.rpm'],],
     }
+
     package {'mcollective-backup-package':
-        require => [File['/root/mcollective/mcollective-backup-package-1.0-1.noarch.rpm'],],
-        ensure => installed,
-        source => "/root/mcollective/mcollective-backup-package-1.0-1.noarch.rpm",
-        provider => rpm,
-        notify => Service['mcollective'],
+        ensure      => installed,
+        source      => '/root/mcollective/mcollective-backup-package-1.0-1.noarch.rpm',
+        provider    => rpm,
+        notify      => Service['mcollective'],
+        require     => File['/root/mcollective/mcollective-backup-package-1.0-1.noarch.rpm'],
     }
 
     Package['mcollective-backup-app'] -> Package['mcollective-backup-agent'] -> Package['mcollective-backup-common'] -> Package['mcollective-backup-package']
