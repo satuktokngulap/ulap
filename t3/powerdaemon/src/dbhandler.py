8ef5835caf07a1a7e97090105866d488f9fd6659
gene <gene.quevedo@gmail.com>
2014-08-15 14:06:36 +0800

    [PM] New ThinClient DB Handler
    for Database transaction

--- t3/powerdaemon/src/dbhandler.py
+++ t3/powerdaemon/src/dbhandler.py
@@ -3,6 +3,8 @@
 
 import sqlite3
 
+from powermodels import ThinClient
+
 class DBHandler():
 
 	#sqlite connection object
@@ -13,3 +15,64 @@ class DBHandler():
 	def openDB(cls, dbfile):
 		cls.conn = sqlite3.connect(dbfile) 
 		cls.cursor = cls.conn.cursor()
+
+	@classmethod
+	def insert(cls, table, data):
+		numvalues = len(data)
+		query = "INSERT INTO %s VALUES (" % table 
+		for n in range(numvalues):
+			query = query+"?,"
+		query = query+")"
+		cls.cursor.execute(query,data)
+
+	@classmethod
+	def update(cls, table, data):
+		pass
+
+	@classmethod
+	def close():
+		pass
+
+class ThinClientHandler(DBHandler):
+
+
+	@classmethod
+	def updateThinClientWithPort(cls, ipaddress, port):
+		data = {"ipaddress": ipaddress , "portnum": port }
+		query = "UPDATE thinclient SET \
+			ipaddress=:ipaddress \
+			WHERE portnum=:portnum"
+
+		cls.cursor.execute(query, data)
+
+	#recieves thinclient object
+	@classmethod
+	def addThinClient(cls, thinclient):
+		data = (thinclient.ipAddress, thinclient.macAddress, thinclient.port)
+
+		cls.insert("thinclient",data)
+
+	@classmethod
+	def removeThinClient(cls, thinclient):
+		query = "DELETE FROM thinclient \
+			WHERE portnum=:portnum"
+		data = {"portnum": thinclient.port}
+
+		cls.cursor.execute(query, data)
+
+	@classmethod
+	def getThinClient(cls, port):
+		query = "SELECT * FROM thinclient WHERE portnum=:portnum"
+		tc = None
+		TClist = cls.cursor.execute(query, {"portnum": port})
+		print TClist
+		for row in TClist:
+			if row[0] == port:
+				tc = ThinClient((str(row[1]), str(row[2])), row[0])
+				break
+		return tc
+
+
+
+
+
--- t3/powerdaemon/test/test_dbhandler.py
+++ t3/powerdaemon/test/test_dbhandler.py
@@ -4,13 +4,19 @@ from mock import Mock, call, patch, MagicMock
 from twisted.internet import defer, reactor
 from twisted.trial import unittest
 
-import dbhandler, sqlite3
+import dbhandler
+import sqlite3
+
+from powermodels import ThinClient
+
 
 class DBHandlertestSuite(unittest.TestCase):
 
 	def setUp(self):
 		self.dbfile = '/users/genepaulquevedo/ulap/t3\
 			/powerdaemon/test/artifacts/testdb.sql'
+		dbhandler.DBHandler.cursor = Mock()
+
 
 	def tearDown(self):
 		reload(dbhandler)
@@ -30,30 +36,122 @@ class DBHandlertestSuite(unittest.TestCase):
 	testCloseDB.skip = "not implemented"
 
 	def testInsert(self):
-		dbhandler.DBHandler.cursor = Mock()
 		table = 'sampletable'
 		data = ('sampledata', 1, 2.3)
-		query = """
-			INSERT into ? VALUES (?,)
-			"""
+		placeholder = "?,?,?,"
+		dbhandler.DBHandler.cursor = Mock()
+		query = "INSERT INTO sampletable VALUES (?,?,?,)"
 
 		#assumes correct table, format and data	
-		dbhandler.DBHandler.insertModel(table, data)
+		dbhandler.DBHandler.insert(table, data)
 
-		cursor.execute.assert_called_with()
+		dbhandler.DBHandler.cursor.execute.assert_called_with(query, data)
 
-	def testUpdate(self):
-		pass
+	# def testUpdate(self):
+	# 	table = 'sampletable'
+	# 	columns = ('name', 'age', 'amount')
+	# 	data = ('sampledata', 1, 2.3)
+	# 	values = None # to be edited
 
-	def testRetrieve(self):
-		pass
+	# 	query = """
+	# 		UPDATE TABLE sampletable
+	# 		SET name=(?) age=(?) amount=(?)
+	# 		WHERE name=(?)
+	# 		"""
+	# 	dbhandler.DBHandler.updateModel(table, columns, data)
 
-	def testCommit(self):
-		pass
+	# 	cursor.execute.assert_called_with(query, values)
 
-	def testConvertThinClient(self):
-		pass
 
-	def testAdapatThinClient(self):
-		pass
+	# def testRetrieveByPrimary(self):
+	# 	table = 'sampletable'
+	# 	column = 'amount'
+	# 	data = 2.65
+	# 	values = None
+
+	# 	query = """
+	# 		SELECT * FROM sampletable WHERE amount=(?)
+	# 		"""
+	# 	dbhandler.DBHandler.updateModel(table, column, data)
+
+	# 	cursor.execute.assert_called_with(query, values)
+
+
+	# def testSelect(self):
+	# 	pass
+
+	# def testCommit(self):
+	# 	pass
+
+	# def testConvertThinClient(self):
+	# 	pass
+
+	# def testAdapatThinClient(self):
+	# 	pass
+
+class ThinClientHandlerTestSuite(unittest.TestCase):
+
+
+	def setUp(self):
+		dbhandler.DBHandler.cursor = Mock()
+
+	def tearDown(self):
+		reload(dbhandler)
+
+	def testUpdateThinClientWithPort_IPAddress(self):
+		query = "UPDATE thinclient SET \
+			ipaddress=:ipaddress \
+			WHERE portnum=:portnum"
+		port = 11
+		ipaddress ="10.234.1.2"
+		data = {"ipaddress": ipaddress, "portnum": port}
+
+		dbhandler.ThinClientHandler.updateThinClientWithPort(ipaddress, port)
+
+		dbhandler.DBHandler.cursor.execute.assert_called_with(query, data)
+
+	def testAddThinClient(self):
+		dbhandler.DBHandler.insert = Mock()
+		thinclient = Mock()
+		thinclient.ipAddress = '10.18.221.218'
+		thinclient.port = 16
+		thinclient.macAddress = 'as:12:we:34:rt:23'
+		data = (thinclient.ipAddress, thinclient.macAddress, thinclient.port)
+
+		dbhandler.ThinClientHandler.addThinClient(thinclient)
+
+		dbhandler.DBHandler.insert.assert_called_with('thinclient', data)
+
+	def testRemoveThinClient_byPort(self):
+		thinclient = Mock()
+		thinclient.ipAddress = '10.18.221.218'
+		thinclient.port = 16
+		thinclient.macAddress = 'as:12:we:34:rt:23'
+		query = "DELETE FROM thinclient \
+			WHERE portnum=:portnum"
+		data = {"portnum": thinclient.port}
+
+		dbhandler.ThinClientHandler.removeThinClient(thinclient)
+
+		dbhandler.DBHandler.cursor.execute.assert_called_with(query, data)
+
+	@patch('dbhandler.ThinClient')
+	def testGetThinClient_byPort(self, thinclient):
+		query = "SELECT * FROM thinclient WHERE portnum=:portnum"
+		port = 5
+		row = [(5, u'10.25.124.111', u'as:12:we:34:rt:23')]
+		ipaddress = '10.25.124.111'
+		macaddress = 'as:12:we:34:rt:23'
+		dbhandler.DBHandler.cursor.execute = MagicMock()
+		dbhandler.DBHandler.cursor.execute.return_value = row		
+		# dbhandler.DBHandler.cursor.execute.__iter__ = Mock()
+		# dbhandler.DBHandler.cursor.execute().next.return_value = row
+
+		tc = dbhandler.ThinClientHandler.getThinClient(port)
+
+		thinclient.assert_called_with((ipaddress, macaddress), port)
+
+
+
+
 
