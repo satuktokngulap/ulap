fbb747d6018efeac284ce808402daf100aa90b09
Belay Montes <iambelay@gmail.com>
2014-09-09 15:45:39 +0800

    Merge start_vm.sh test to master

--- t3/kickstarter/preliminaries/start_vm.sh
+++ t3/kickstarter/preliminaries/start_vm.sh
@@ -5,8 +5,43 @@ LOG="logger -s $0:"
 TIMEOUT=3600
 LOCALNODE="`uname -n | gawk -F'.' '{print $1}'`"
 
-$LOG "Sleep for 1 minute to wait for storage service to stabilize"
+$LOG "Sleep for 1 minute to wait for services to stabilize"
 sleep 60
+
+TIMER=0
+$LOG "Checking if cman/rgmanager/drbd is already up..."
+while [ $TIMER -ne $TIMEOUT ]; do
+	#check if cman is started
+	service cman status
+	if [ $? -eq 0 ]; then
+		#check if rgmanager is started
+		service rgmanager status
+		if [ $? -eq 0 ]; then
+			# check if drbd is started
+			service drbd status
+			if [ $? -eq 0 ]; then
+				$LOG "cman/rgmanager/drbd are up!"
+				break
+			else
+				# if not started, sleep 3 seconds
+				sleep 3
+				TIMER=$(( $TIMER + 3 ))
+			fi
+		else
+			# if not started, sleep 3 seconds
+			sleep 3
+			TIMER=$(( $TIMER + 3 ))
+		fi
+	else
+		sleep 3
+		TIMER=$(( $TIMER + 3 ))
+	fi
+done
+
+if [ $TIMER -eq $TIMEOUT ]; then
+	$LOG "Timed out!"
+	exit 1
+fi
 
 TIMER=0
 $LOG "Checking status of storage service..."
@@ -19,10 +54,10 @@ while [ $TIMER -ne $TIMEOUT ]; do
 			clusvcadm -e nfs-shared
 			$LOG "Checking status of VM services on $LOCALNODE"
 			for VM in $(clustat | gawk '!/Service/ && !/-/ && /vm:'${LOCALNODE:1:1}'_/ {print $1}'); do
-                                if [ "$VM" == "vm:a_vm_rdpa"  -o  "$VM" == "vm:b_vm_rdpb" ]; then
-					$LOG "$VM is marked for exclusion. Will not enable this service."
-					continue
-				fi
+                                #if [ "$VM" == "vm:a_vm_rdpa"  -o  "$VM" == "vm:b_vm_rdpb" ]; then
+				#	$LOG "$VM is marked for exclusion. Will not enable this service."
+				#	continue
+				#fi
 				# Check status of VMs
 				case $(clustat -s $VM | gawk '!/Service/ && !/-/ {print $3}') in
 				started )
@@ -50,10 +85,6 @@ while [ $TIMER -ne $TIMEOUT ]; do
 			done
 			break
 			;;
-		"" )
-			$LOG "No storage service for node ${LOCALNODE}"
-			break
-			;;
 		*)
 			sleep 3
 			TIMER=$(( $TIMER + 3 ))
@@ -61,9 +92,15 @@ while [ $TIMER -ne $TIMEOUT ]; do
 	esac
 done
 
-if [ "$(clustat -s storage_$LOCALNODE | gawk '!/Service/ && !/-/ {print $3}')" != "started" ]; then
-	$LOG "Timed out waiting for storage_$LOCALNODE to start!"
-	res=1
+
+if [ $TIMER -eq $TIMEOUT ]; then
+	$LOG "Timed out!"
+	exit 1
 fi
 
-exit $res
+if [ "$(clustat -s storage_$LOCALNODE | gawk '!/Service/ && !/-/ {print $3}')" != "started" ]; then
+	$LOG "storage_$LOCALNODE is not started!"
+	exit 2
+fi
+
+exit 0
